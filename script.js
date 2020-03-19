b12e1a9ba7da2b4f4398b52887ad3a6e9abe06f8
drjurgen <nicoj98@gmail.com>
2020-03-19 15:33:27 +0100

    ændring pt 1

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-let cube = document.querySelector('.cube');
-let radioGroup = document.querySelector('.radio-group');
-let currentClass = '';
 
-function changeSide() {
-    let checkedRadio = radioGroup.querySelector(':checked');
-    let showClass = 'show-' + checkedRadio.value;
-    if (currentClass) {
-        cube.classList.remove(currentClass);
-    }
-    cube.classList.add(showClass);
-    currentClass = showClass;
-}
-
-changeSide();
-
-radioGroup.addEventListener('change', changeSide);
